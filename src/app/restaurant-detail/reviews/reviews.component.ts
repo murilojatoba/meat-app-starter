47abd547f389bcb2ba42abd17e31603d7697d737
Murilo Jatobá <murilojatoba@gmail.com>
2017-11-08 22:01:09 -0300

    Primeiro commit

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+
+import { RestaurantsService } from '../../restaurants/restaurants.service';
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  reviews: Observable<any>;
+
+  constructor(
+    private restaurantService: RestaurantsService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.reviews = this.restaurantService.reviewsOfRestaurant(this.activatedRoute.parent.snapshot.params['id']);
+  }
+
+}
---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Restaurant } from './restaurant/restaurant.model';
+import { RestaurantsService } from './restaurants.service';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[];
+
+  constructor(private restaurantsServices: RestaurantsService) { }
+
+  ngOnInit() {
+      this.restaurantsServices.restaurants()
+        .subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+}
